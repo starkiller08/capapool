52c16d9fab404608d3d100517dbe9b972db87182
sarimkhan752 <45905242+LazarusFox7@users.noreply.github.com>
2026-05-21 03:19:42 +1000

    Fixed configuration for PostgreSQL database connection

--- capapool/settings.py
+++ capapool/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "capapool.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "capapool_db",
+        "USER": "at0m",
+        "PASSWORD": "capa00l_talent",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
