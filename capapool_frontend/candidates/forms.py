64a46779a804bdfb7d9d16145fbdabfb499de9ee
sarimkhan752 <45905242+LazarusFox7@users.noreply.github.com>
2026-06-05 21:42:34 +1000

    Final checks and fixes, preparing for final submission

--- capapool_frontend/candidates/forms.py
+++ capapool_frontend/candidates/forms.py
@@ -3,6 +3,8 @@ from .models import CandidateProfile
 
 
 class CandidateProfileForm(forms.ModelForm):
+	skills = forms.CharField(required=False, label='skills', help_text='Enter skills separated by commas, e.g. Python, Django, etc.')
+
 	class Meta:
 		model = CandidateProfile
 
@@ -26,5 +28,45 @@ class CandidateProfileForm(forms.ModelForm):
 				'rows': 4,
 				'placeholder': 'Briefly describe your past work experience....'
 			}),
-			'skills': forms.CheckboxSelectMultiple(),
+			#'skills': forms.CheckboxSelectMultiple(),
 		}
+
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		if self.instance and self.instance.pk:
+			current_skills = self.instance.skills.all()
+			skill_names = []
+
+			for skill in current_skills:
+				skill_names.append(skill.name)
+
+			self.fields['skills'].initial = ', '.join(skill_names)
+
+
+	def save(self, commit=True):
+		candidate = super().save(commit=False)
+
+
+		if commit:
+			candidate.save()
+
+            # Clear old skills first, then add the updated skills
+			candidate.skills.clear()
+
+			skills_text = self.cleaned_data.get('skills', '')
+
+			if skills_text:
+				skill_names = skills_text.split(',')
+
+				for skill_name in skill_names:
+					clean_skill_name = skill_name.strip()
+
+					if clean_skill_name:
+						skill, created = Skill.objects.get_or_create(
+							name=clean_skill_name
+						)
+						candidate.skills.add(skill)
+
+		return candidate
--- capapool_frontend/capapool/settings.py
+++ capapool_frontend/capapool/settings.py
@@ -79,23 +79,24 @@ WSGI_APPLICATION = "capapool.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "capapool_db",
-#         "USER": "at0m",
-#         "PASSWORD": "capa00l_talent",
-#         "HOST": "localhost",
-#         "PORT": "5432"
-#     }
-# }
-
 DATABASES = {
+
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "capapool_db",
+        "USER": "at0m",
+        "PASSWORD": "capa00l_talent",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 
 # Password validation
